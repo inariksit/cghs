e35692a2916dcbc32901a5e93235d7b0571e45f1
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-12-15 11:55:30 +0100

    fix tabs. (TODO this still doesn't work.)

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,10 @@
 import Distribution.Simple
 import System.Process ( callProcess )
 
-main = do callProcess "bnfc"  ["-d" "src/bnfc/CG.cf"]
-          callProcess "happy" ["-gca" "src/CG/Par.y"]
-	      callProcess "alex"  ["-g" "src/CG/Lex.x"]
-	      defaultMain
+main = do callProcess "bnfc"  ["-d", "src/bnfc/CG.cf"]
+          callProcess "happy" ["-gca", "src/CG/Par.y"]
+          callProcess "alex"  ["-g", "src/CG/Lex.x"]
+          defaultMain
 
 --main = defaultMainWithHooks simpleUserHooks { preConf = runBNFC }
 -- where runBNFC = undefined
