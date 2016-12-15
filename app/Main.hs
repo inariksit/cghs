de2c7422694cf33bbaeabbc50c2e11ac80c7a804
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-12-15 11:55:11 +0100

    stuff

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,10 @@ import System.Environment ( getArgs )
 main :: IO ()
 main = do args <- getArgs
           case args of
-            [rls] -> print =<< fmap parseRules (readFile rls)
---            		     (parseRules rules)
+            [rls] -> do text <- readFile rls 
+                        let (defs,rules) = parseRules text
+                        mapM_ print defs
+                        putStrLn ""
+                        mapM_ (mapM_ print) rules
             _     -> error "Usage: stack exec read-cg <rules.rlx>"
 
