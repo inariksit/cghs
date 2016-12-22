74f8faedeb58a915662c5a71864fafdfe678a445
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-12-22 19:55:57 +0100

    terrible workarounds for mystical bugs, maybe in BNFC, maybe in my grammar?

--- app/Main.hs
+++ app/Main.hs
@@ -9,9 +9,14 @@ main :: IO ()
 main = do args <- getArgs
           case args of
             [rls] -> do text <- readFile rls 
-                        let (defs,rules) = parse True text
+                        let text' = unlines $ map replaceComment $ lines text
+                        let (defs,rules) = parse text'
                         mapM_ print defs
                         putStrLn ""
                         mapM_ (mapM_ print) rules
             _     -> error "Usage: stack exec read-cg <rules.rlx>"
 
+
+replaceComment :: String -> String
+replaceComment ('#':_) = []
+replaceComment x       = x
