5fd8673f41a3b8b7724079a54700504b16243f79
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-02-16 11:01:05 +0100

    trying out new functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import CGHS ( Rule(..), TagSet, parse )
+import CGHS ( Rule(..), TagSet, parse, groupRules, sortByContext )
+import CGHS.Clean ( findRepetition, compactTagset, compactRule ) 
 import System.Environment ( getArgs )
 
 
@@ -10,7 +11,48 @@ main = do args <- getArgs
             [rls] -> do --E.setLocaleEncoding E.utf8 --maybe I'll need it later?
                         text <- readFile rls 
                         let (defs,rules) = parse text
-                        mapM_ print defs
+                        --mapM_ print defs
                         putStrLn ""
-                        mapM_ (mapM_ print) rules
+                        let compactDefs = [ compactTagset def
+                                            | (name,def) <- defs ]
+                        let diffDefs = [ (nm,odef,comp) 
+                                        | ((nm,odef),comp) <- zip defs compactDefs
+                                        , odef /= comp ]
+                        mapM_ (\(nm,def,c) -> do putStrLn "\nOriginal definition:"
+                                                 putStrLn (nm ++ " = " ++ show def)
+                                                 putStrLn "Compacted some tagsets:"
+                                                 print c
+                              ) diffDefs
+
+                        let repDefs = [ (,) (nm,def) [ (rd,occs)
+                                          | (rd,occs) <- findRepetition def 
+                                          , occs > 2 ]
+                                        | (nm,def) <- defs ]
+                        --mapM_ (mapM_ print) rules
+                        mapM_ (\((nm,def),occs) -> case occs of
+                                 [] -> return ()
+                                 _  ->  do putStrLn "\nOriginal definition:"
+                                           putStrLn (nm ++ " = " ++ show def)
+                                           putStrLn "\nRepetitive elements:"
+                                           mapM_ (\(x,y) -> 
+                                                  putStrLn (show x ++ ": " ++ show y)) occs
+                                           putStrLn "-----\n"
+                              ) repDefs
+
+                        putStrLn "\n\n\n"
+                        let compRules = map compactRule (concat rules)
+                        let diffRules = [ show orig ++ "\n\t" ++  show comp ++ "\n"
+                                           | (orig,comp) <- zip (concat rules) compRules
+                                           , orig /= comp ]
+                        --mapM_ putStrLn diffRules
+
+                        --putStrLn "Grouping rules by targets"
+                        --let groupedRlsBySection = map groupRules rules 
+                        --let groupedRls = map (reverse . sortByContext) $
+                        --				  groupRules (concat rules) :: [[Rule]]
+                        ----print (elems groupledRls)
+                        --mapM_ (\x -> mapM_ print x >> putStrLn "\n") groupedRls
+                        putStrLn "done"
             _     -> error "Usage: stack exec read-cg <rules.rlx>"
+
+
